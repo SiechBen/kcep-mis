3e3c8f345c0413232d75e077c8fbe2c1a75cefdf
siech <siech@Mi>
2016-05-27 10:25:02 +0300

    Completed CRUD functionality for Equipment and Warehouse. Modified Warehouse and Person entities.

---
+++ src/main/java/ke/co/miles/kcep/mis/entities/Equipment.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ke.co.miles.kcep.mis.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author siech
+ */
+@Entity
+@Table(catalog = "kcep_mis", schema = "", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"id"})})
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Equipment.findAll", query = "SELECT e FROM Equipment e"),
+    @NamedQuery(name = "Equipment.findById", query = "SELECT e FROM Equipment e WHERE e.id = :id"),
+    @NamedQuery(name = "Equipment.findByType", query = "SELECT e FROM Equipment e WHERE e.type = :type"),
+    @NamedQuery(name = "Equipment.findByEquipmentNumber", query = "SELECT e FROM Equipment e WHERE e.equipmentNumber = :equipmentNumber"),
+    @NamedQuery(name = "Equipment.findByStatus", query = "SELECT e FROM Equipment e WHERE e.status = :status")})
+public class Equipment implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Integer id;
+    @Size(max = 45)
+    @Column(length = 45)
+    private String type;
+    @Size(max = 45)
+    @Column(name = "equipment_number", length = 45)
+    private String equipmentNumber;
+    @Size(max = 45)
+    @Column(length = 45)
+    private String status;
+    @JoinColumn(name = "warehouse", referencedColumnName = "id", nullable = false)
+    @ManyToOne(optional = false)
+    private Warehouse warehouse;
+
+    public Equipment() {
+    }
+
+    public Equipment(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getEquipmentNumber() {
+        return equipmentNumber;
+    }
+
+    public void setEquipmentNumber(String equipmentNumber) {
+        this.equipmentNumber = equipmentNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Warehouse getWarehouse() {
+        return warehouse;
+    }
+
+    public void setWarehouse(Warehouse warehouse) {
+        this.warehouse = warehouse;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Equipment)) {
+            return false;
+        }
+        Equipment other = (Equipment) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ke.co.miles.kcep.mis.entities.Equipment[ id=" + id + " ]";
+    }
+    
+}
